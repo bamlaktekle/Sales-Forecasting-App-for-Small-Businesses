8da1c1cd93cdd0c8003d2ca6f74b8208299e2583
Salemata Diallo <sd5584@nyu.edu>
2025-10-15 13:14:32 +0000

    change title

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import streamlit as st, pandas as pd, matplotlib.pyplot as plt, seaborn as sns
 
-st.title("📊 Data Visualization & Insights")
+st.title("Sales Forecasting App for Small Business")
 df = pd.read_csv("data/Advertising_Data.csv")
 st.dataframe(df.head())
